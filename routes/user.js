7613207c1f8ecb31c92c75eafa75ac274c356b3e
gamwang <jonbai23@gmail.com>
2014-07-20 01:53:12 -0700

    Implemented User Authentication and User Signup as well as creating/mapping unique visitor session with a unique session id.

--- routes/user.js
+++ routes/user.js
@@ -9,13 +9,17 @@ var generate_key = function(req, res, next) {
     var sha = crypto.createHash('sha256');
     sha.update(Math.random().toString());
     out = sha.digest('hex');
-    req.session.sid = out;
+    req.session.sid = out[0];
+    for (var i = 1; i < 32; i += 1) {
+        req.session.sid += out[i];
+    }
 };
 
 var check_session = function(req, res, next) {
     if (!req.session.sid) {
         generate_key(req, res, next);
     }
+    console.log("user session id: " + req.session.sid);
 };
 
 var user = {
@@ -26,24 +30,31 @@ var user = {
     home: function(req, res, next) {
         check_session(req, res, next);
         req.session.lastPage = '/home';
-        res.send("welcome, " + req.session.user + "!");
+        var show = 'Not Logged In';
+        if (req.session.user) {
+            show = req.session.user;
+        }
+        res.render('index', {
+            name: show
+        });
     },
     login: function(req, res, next) {
         check_session(req, res, next);
         var body = req.body;
-        client.get(body.email, function(err, password) {
-            if (body.password == password) {
-                req.session.user = body.email;
-                res.redirect('/home');
-            } else {
+        client.hget('users', body.email, function(err, data) {
+            data = JSON.parse(data);
+            if (_.isNull(data) || body.password != data.psswrd) {
                 res.redirect('/');
+            } else {
+                req.session.user = data.frstnme + ' ' + data.lstnme;
+                res.redirect('/home');
             }
         });
     },
     signup: function(req, res, next) {
         check_session(req, res, next);
         var body = req.body;
-        client.set(body.name, body);
+        client.hset('users', body.email, JSON.stringify(body));
         res.redirect('/');
     },
     getVideo: function(req, res, next) {
