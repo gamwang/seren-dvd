2f668ca376bde6f70e9ac9e85aa9b0af8ea84633
gamwang <jonbai23@gmail.com>
2014-07-26 19:54:29 -0700

    Added Authentication support with passport.js and chat service displays user's first name. Also, browser_sessions are now stored in redis.

--- routes/user.js
+++ routes/user.js
@@ -7,80 +7,89 @@ var crypto = require('crypto');
 
 var ip_cache = {};
 
-var generate_key = function(req, res, next) {
-    var sha = crypto.createHash('sha256');
-    var out;
-    sha.update(Math.random().toString());
-    out = sha.digest('hex');
-    req.session.sid = out[0];
-    for (var i = 1; i < 20; i += 1) {
-        req.session.sid += out[i];
-    }
-};
+// var generate_key = function(req, res, next) {
+//     var sha = crypto.createHash('sha256');
+//     var out;
+//     sha.update(Math.random().toString());
+//     out = sha.digest('hex');
+//     req.session.sid = out[0];
+//     for (var i = 1; i < 20; i += 1) {
+//         req.headers.cookie.sid += out[i];
+//     }
+// };
 
-var check_session = function(req, res, next) {
-    if (!req.session.sid) {
-        generate_key(req, res, next);
-    }
-    // console.log("user session id: " + req.session.sid);
-    // console.log("user session Information: " + JSON.stringify(req.session));
-};
+// var check_session = function(req, res, next) {
+//     if (!req.session.sid) {
+//         generate_key(req, res, next);
+//     }
+//     // console.log("user session id: " + req.session.sid);
+//     // console.log("user session Information: " + JSON.stringify(req.session));
+// };
 
-var store_location = function(req, res, next) {
-    var ip = req.headers['x-real-ip']
-    request('http://freegeoip.net/json/' + ip, function(err, res, body) {
-        countryName = JSON.parse(body).country_name.toString();
-        console.log(country_name);
-    });
-}
+// var store_location = function(req, res, next) {
+//     var ip = req.headers['x-real-ip']
+//     request('http://freegeoip.net/json/' + ip, function(err, res, body) {
+//         try {
+//             countryName = JSON.parse(body).country_name.toString();
+//             console.log(countryName);
+//         } catch (e) {
+//             console.log(e);
+//         }
+//     });
+// }
 
 var user = {
     index: function(req, res, next) {
-        store_location(req, res, next);
-        check_session(req, res, next);
-        console.log("SESSION:" + JSON.stringify(req.session));
+        var ip = req.headers['x-real-ip'];
+        var time = new Date().getTime();
+        var info = {
+            ip: ip || 'none',
+            ms: time
+        }
+        client.hset('browser_sessions:' + req.sessionID, time, JSON.stringify(info));
+
+        var name = null
+        if (req.user) {
+            name = req.user.frstnme
+        }
         res.render('index', {
-            name: req.session.user
+            name: name
         });
     },
     home: function(req, res, next) {
-        check_session(req, res, next);
-        var show = 'Not Logged In';
-        if (req.session.user) {
-            show = req.session.user;
+        var show = null;
+        if (req.user) {
+            show = req.user.frstnme;
         }
         res.render('index', {
             name: show
         });
     },
-    login: function(req, res, next) {
-        check_session(req, res, next);
-        var sha = crypto.createHash('sha256');
-        var body = req.body;
-        console.log(body);
-        sha.update(body.password.toString());
-        var cmp_psswrd = sha.digest('hex');
-        client.hget('users', body.email, function(err, data) {
-            data = JSON.parse(data);
-            if (!_.isNull(data) && cmp_psswrd == data.psswrd) {
-                req.session.user = data.frstnme + ' ' + data.lstnme;
-            }
-            res.redirect('/');
-        });
-    },
-    logout: function(req, res, next) {
-        delete req.session.user
-        res.redirect('/')
-    },
     signup: function(req, res, next) {
-        check_session(req, res, next);
         var sha = crypto.createHash('sha256');
         var body = req.body;
-        console.log(body);
         sha.update(body.psswrd.toString());
         body.psswrd = sha.digest('hex');
-        client.hset('users', body.email, JSON.stringify(body));
-        res.redirect('/');
+        body.email = body.email.toLowerCase();
+        client.hget('users', body.email, function(err, data) {
+            data = JSON.parse(data);
+            if (_.isNull(data)) {
+                client.hset('users', body.email, JSON.stringify(body));
+                res.redirect('/');
+            } else {
+                res.redirect('/tryagain');
+            }
+        });
+    },
+    signupFail: function(req, res, next) {
+        res.render('error', {
+            msg: 'Email already exists in the Database! Try a different one!'
+        });
+    },
+    loginFail: function(req, res, next) {
+        res.render('error', {
+            msg: 'wrong login information!'
+        });
     },
     getVideo: function(req, res, next) {
         console.log(req.body.video_query);
--- web.js
+++ web.js
@@ -5,12 +5,59 @@ var path = require('path');
 var multer = require('multer');
 var cookieParser = require('cookie-parser');
 var session = require('express-session');
+var redis = require('redis');
+var socket = require('socket.io');
+var passportSocketIo = require("passport.socketio");
+var cookie = require("cookie");
+var connect = require('connect');
+var crypto = require('crypto');
+var _ = require('underscore');
+var RedisStore = require('connect-redis')(session);
 
 var user = require('./routes/user');
 
+var sessionStore = new RedisStore;
+
 var app = express();
 var http = require('http').Server(app);
-var io = require('socket.io')(http);
+
+var client = redis.createClient();
+// passport authentication
+var passport = require('passport'),
+    LocalStrategy = require('passport-local').Strategy;
+
+passport.use(new LocalStrategy({
+        usernameField: 'email',
+        passwordField: 'password'
+    },
+    function(email, password, done) {
+        var sha = crypto.createHash('sha256');
+        sha.update(password.toString());
+        var cmp_psswrd = sha.digest('hex');
+        email = email.toLowerCase();
+        client.hget('users', email, function(err, data) {
+            data = JSON.parse(data);
+            if (_.isNull(data)) { // if user name does not exist
+                return done(null, false, {
+                    msg: 'Incorrect username.'
+                });
+            } else if (cmp_psswrd != data.psswrd) { // if passwrod does not match
+                return done(null, false, {
+                    msg: 'Incorrect password.'
+                });
+            } else { // success login
+                done(null, data);
+            }
+        });
+    }
+));
+passport.serializeUser(function(user, done) {
+    done(null, user);
+});
+
+passport.deserializeUser(function(user, done) {
+    done(null, user);
+});
 
 // In order to track of req's body. ;)
 app.use(bodyParser.urlencoded({
@@ -25,10 +72,16 @@ app.use(multer({
 // In order to track sessions and cookies
 app.use(cookieParser());
 app.use(session({
+    key: 'connect.sid',
     resave: false, // don't save session if unmodified
     saveUninitialized: false, // don't create session until something stored
-    secret: 'shhhh, very secret'
+    secret: 'JBai23',
+    store: sessionStore
 }));
+
+//for passport.js authentication
+app.use(passport.initialize());
+app.use(passport.session());
 
 //setup views and path
 app.set('views', path.join(__dirname, 'views'));
@@ -39,30 +92,48 @@ app.set('view engine', 'jade');
 //Routes
 app.get('/', user.index);
 app.get('/home', user.home);
-app.post('/login', user.login);
-app.post('/logout', user.logout);
+app.get('/signupFail', user.signupFail);
+app.get('/loginFail', user.loginFail);
+app.post('/login', passport.authenticate('local', {
+    successRedirect: '/',
+    failureRedirect: '/loginFail'
+}));
+app.get('/logout', function(req, res) {
+    req.logout();
+    res.redirect('/');
+});
 app.post('/signup', user.signup);
 app.post('/getVideo', user.getVideo);
 
 
 //Chat Service
+var io = socket(http);
+
+io.use(passportSocketIo.authorize({
+    cookieParser: cookieParser,
+    key: 'connect.sid',
+    secret: 'JBai23',
+    store: sessionStore,
+    success: function onAuthorizeSuccess(data, accept) {
+        console.log('successful connection to socket.io');
+        // The accept-callback still     allows us to decide whether to
+        // accept the connection or not.
+        accept(null, true);
+    },
+    fail: function onAuthorizeFail(data, message, error, accept) {
+        if (error)
+            throw new Error(message);
+        console.log('failed connection to socket.io:', message);
+        // We use this callback to log all of our failed connections.
+        accept(null, false);
+    }
+}));
+
 io.on('connection', function(socket) {
+    var req = socket.conn.request;
     socket.on('chat message', function(msg) {
-        io.emit('chat message', msg);
+        io.emit('chat message', req.user.frstnme + ": " + msg);
     });
-});
-
-io.set('authorization', function(data, accept) {
-    // console.log(data.headers);
-    // if (data.headers.cookie) {
-
-    //     data.cookie = cookieParser(data.headers.cookie);
-    //     data.sessionID = data.cookie['connect.sess'].split('.')[0].substring(2);
-
-    //     sessionStore.get(data.sessionID, function(err, session) {
-    //         // now you have all session variables
-    //     });
-    // }
 });
 
 var server = http.listen(3000, function() {
